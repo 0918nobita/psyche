7b685c17fb4448081850aca45eeb1a18a7047f49
0918nobita <nobita.0918@gmail.com>
2019-05-19 16:05:04 +0900

    :building_construction: bin 型の定義の修正に伴って、bin_of_unsigned 関数を再定義した

--- binary.ml
+++ binary.ml
@@ -1,8 +1,8 @@
 module Binary :
   sig
-    val bin_of_unsigned : int -> int -> bin
     type bit = Zero | One
     type bin = bit list
+    val bin_of_unsigned : int -> int -> bit list
     val string_of_bin : bin -> string
   end
 =
@@ -13,18 +13,18 @@ module Binary :
     let make_list len elem = Array.to_list @@ Array.make len elem
 
     let bin_of_unsigned size = function
-      | 0 -> Bin (make_list size 0)
+      | 0 -> (make_list size Zero)
       | n ->
           let rec conv = function
-            | (0, bin) -> (0, bin)
-            | (decimal, bin) ->
-                conv (decimal / 2, (decimal mod 2) :: bin)
-          in
-            let (_, bin) = conv (n, []) in
-            let lack = size - List.length bin in
-              if lack >= 0
-                then Bin ((make_list lack 0) @ bin)
-                else failwith "(bin_of_int) Overflow"
+              | (0, bin) -> (0, bin)
+              | (decimal, bin) ->
+                  conv (decimal / 2, (if decimal mod 2 = 0 then Zero else One) :: bin)
+            in
+              let (_, bin) = conv (n, []) in
+              let lack = size - List.length bin in
+                if lack >= 0
+                  then (make_list lack Zero) @ bin
+                  else failwith "(bin_of_int) Overflow"
 
     let string_of_bin bits =
       String.concat ""
