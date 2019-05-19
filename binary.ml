305ff382fd4f54458ced2d040c4df320fd2619bc
0918nobita <nobita.0918@gmail.com>
2019-05-19 16:05:28 +0900

    :sparkles: Binary.plus 関数を追加した

--- binary.ml
+++ binary.ml
@@ -2,6 +2,7 @@ module Binary :
   sig
     type bit = Zero | One
     type bin = bit list
+    val plus : int -> bit list -> bit list -> bit list
     val bin_of_unsigned : int -> int -> bit list
     val string_of_bin : bin -> string
   end
@@ -11,6 +12,31 @@ module Binary :
     type bin = bit list
 
     let make_list len elem = Array.to_list @@ Array.make len elem
+
+    let plus size a b =
+      let rec plus_inner = function
+        | (false, [], [], bits) -> bits
+        | (false, bit :: a, [], c) -> bit :: c
+        | (false, [], bit :: b, c) -> bit :: c
+        | (false, Zero :: a, Zero :: b, c) -> plus_inner (false, a, b, Zero :: c)
+        | (false, Zero :: a, One :: b, c) -> plus_inner (false, a, b, One :: c)
+        | (false, One :: a, Zero :: b, c) -> plus_inner (false, a, b, One :: c)
+        | (false, One :: a, One :: b, c) -> plus_inner (true, a, b, Zero :: c)
+        | (true, [], [], c) -> One :: c
+        | (true, Zero :: a, [], c) -> One :: c
+        | (true, One :: a, [], c) -> One :: Zero :: c
+        | (true, [], Zero :: b, c) -> One :: c
+        | (true, [], One :: b, c) -> One :: Zero :: c
+        | (true, Zero :: a, Zero :: b, c) -> plus_inner (false, a, b, One :: c)
+        | (true, Zero :: a, One :: b, c) -> plus_inner (true, a, b, Zero :: c)
+        | (true, One :: a, Zero :: b, c) -> plus_inner (true, a, b, Zero :: c)
+        | (true, One :: a, One :: b, c) -> plus_inner (true, a, b, One :: c)
+      in
+        let c = plus_inner (false, List.rev a, List.rev b, []) in
+        let lack = size - List.length c in
+          if lack >= 0
+            then (make_list lack Zero) @ c
+            else failwith "(plus) Overflow"
 
     let bin_of_unsigned size = function
       | 0 -> (make_list size Zero)
