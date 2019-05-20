6d8a10035cd4ade798f3977ba3544dcf7b0ea3fc
0918nobita <nobita.0918@gmail.com>
2019-05-20 21:59:09 +0900

    :construction: 固定長/可変長それぞれに対するバイナリ操作関数を追加した

--- src/binary.ml
+++ src/binary.ml
@@ -2,13 +2,17 @@ module Binary :
   sig
     type bit = Zero | One
     type bin = bit list
-    val plus : int -> bit list -> bit list -> bit list
-    val twos_complement : int -> bit list -> bit list
-    val bin_of_int : int -> int -> bit list
+    val fixed_plus : int -> bit list -> bit list -> bit list
+    val fixed_twos_complement : int -> bit list -> bit list
+    val fixed_bin_of_int : int -> int -> bit list
+    val variable_plus : bit list -> bit list -> bit list
+    val variable_twos_complement : bit list -> bit list
+    val variable_bin_of_int : int -> bit list
     val string_of_bin : bin -> string
     val slice : 'a list -> int -> int -> 'a list
     val split_per : int -> 'a list -> ('a list) list
     val int_of_bin : bit list -> int
+    val leb128_of_int : int -> bit list list
   end
 =
   struct
@@ -17,7 +21,7 @@ module Binary :
 
     let make_list len elem = Array.to_list @@ Array.make len elem
 
-    let plus size x y =
+    let fixed_plus size x y =
       let rec plus_inner = function
         | (false, a, b, c) ->
             (match (a, b, c) with
@@ -46,10 +50,38 @@ module Binary :
             then (make_list lack Zero) @ z
             else failwith "(plus) Overflow"
 
-    let twos_complement size bin =
-      plus size [One] @@ List.map (function Zero -> One | One -> Zero) bin
+    let variable_plus x y =
+      let rec plus_inner = function
+        | (false, a, b, c) ->
+            (match (a, b, c) with
+              | ([], [], bits) -> bits
+              | (bit :: a, [], c) -> plus_inner (false, a, [], bit :: c)
+              | ([], bit :: b, c) -> plus_inner (false, [], b, bit :: c)
+              | (Zero :: a, Zero :: b, c) -> plus_inner (false, a, b, Zero :: c)
+              | (Zero :: a, One :: b, c) -> plus_inner (false, a, b, One :: c)
+              | (One :: a, Zero :: b, c) -> plus_inner (false, a, b, One :: c)
+              | (One :: a, One :: b, c) -> plus_inner (true, a, b, Zero :: c))
+        | (true, a, b, c) ->  (* set carry flag *)
+            (match (a, b, c) with
+              | ([], [], c) -> One :: c
+              | (Zero :: a, [], c) -> plus_inner (false, a, [], One :: c)
+              | (One :: a, [], c) -> plus_inner (true, a, [], Zero :: c)
+              | ([], Zero :: b, c) -> plus_inner (false, [], b, One :: c)
+              | ([], One :: b, c) -> plus_inner (true, [], b, Zero :: c)
+              | (Zero :: a, Zero :: b, c) -> plus_inner (false, a, b, One :: c)
+              | (Zero :: a, One :: b, c) -> plus_inner (true, a, b, Zero :: c)
+              | (One :: a, Zero :: b, c) -> plus_inner (true, a, b, Zero :: c)
+              | (One :: a, One :: b, c) -> plus_inner (true, a, b, One :: c))
+      in
+        plus_inner (false, List.rev x, List.rev y, [])
 
-    let rec bin_of_int size = function
+    let fixed_twos_complement size bin =
+      fixed_plus size [One] @@ List.map (function Zero -> One | One -> Zero) bin
+
+    let variable_twos_complement bin =
+      variable_plus [One] @@ List.map (function Zero -> One | One -> Zero) bin
+
+    let rec fixed_bin_of_int size = function
       | 0 -> (make_list size Zero)
       | n when n > 0 ->
           let rec conv = function
@@ -62,7 +94,20 @@ module Binary :
               if lack >= 0
                 then (make_list lack Zero) @ bin
                 else failwith "(bin_of_int) Overflow"
-      | n -> twos_complement size @@ bin_of_int size @@ (-1) * n
+      | n -> fixed_twos_complement size @@ fixed_bin_of_int size @@ (-1) * n
+
+    let rec variable_bin_of_int = function
+      | 0 -> [Zero]
+      | n ->
+          let rec conv = function
+              | (0, bin) -> (0, bin)
+              | (decimal, bin) ->
+                  conv (decimal / 2, (if decimal mod 2 = 0 then Zero else One) :: bin)
+          in
+            let (_, bin) = conv (n, []) in
+              if n >= 0
+                then bin
+                else variable_twos_complement bin
 
     let string_of_bin bits =
       String.concat ""
@@ -82,10 +127,10 @@ module Binary :
     let split_per size arr =
       let len = List.length arr in
       let rec split_per_inner = function
-        | (current, part) when current >= len -> part
-        | (current, part) -> split_per_inner (current + size, slice arr current (current + size - 1) :: part)
+        | (current, part) when current < 0 -> part
+        | (current, part) -> split_per_inner (current - size, slice arr (current - size + 1) current :: part)
       in
-        List.rev @@ split_per_inner (0, [])
+        split_per_inner (len - 1, [])
 
     let int_of_bin bin =
       List.fold_left
@@ -94,4 +139,24 @@ module Binary :
         @@ List.mapi
           (fun i bit -> (int_of_float @@ 2. ** float_of_int i) * (match bit with Zero -> 0 | One -> 1))
           @@ List.rev bin
+
+    let leb128_of_int n =
+      let rec len = function
+        | head :: tail when not @@ List.mem One head -> len tail
+        | bin -> List.length bin
+      in
+      let bytes = split_per 7 @@ fixed_bin_of_int 32 @@ abs n in
+      let
+        piece = len bytes and
+        length = List.length bytes
+      in
+        List.rev
+          @@ List.mapi
+            (fun i byte -> if i = 0 then Zero :: byte else One :: byte)
+            @@ if n >= 0
+              then
+                slice bytes (length - piece) (length - 1)
+              else
+                let twos_complement = split_per 7 @@ fixed_bin_of_int 32 n in
+                  slice twos_complement (length - piece) (length - 1)
   end
