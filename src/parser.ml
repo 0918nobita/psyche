dd97c9a60e1996acbb017d9323f4be1fa0f912f5
0918nobita <nobita.0918@gmail.com>
2019-05-25 22:04:15 +0900

    :art: Improve readability

--- src/parser.ml
+++ src/parser.ml
@@ -55,77 +55,85 @@ let rec factor () target position =
     | _ -> Failure
 
 and term target position =
-  match sequence [lazy_parse factor; many @@ sequence [choice [atom "*"; atom "/"]; lazy_parse factor]] target position with
-    | Success (tokens, _, p) ->
-        (match tokens with
-          | [Ast _] as ast_list -> Success (ast_list, target, p)
-          | Ast (_ as lhs) :: op :: Ast (_ as rhs) :: tail when op = Token "*" || op = Token "/" ->
-              let ast = ref @@ if op = Token "*" then Mul (lhs, rhs) else Div (lhs, rhs) in
-              let rec conv base = function
-                | [] -> ()
-                | op :: Ast (_ as r) :: tail when op = Token "*" || op = Token "/" ->
-                    base := if op = Token "*" then Mul (!base, r) else Div (!base, r);
-                    conv base tail
-                | _ -> failwith "(term) Fatal Error"
-              in
-                conv ast tail;
-                Success ([Ast !ast], target, p)
-          | _ -> Failure)
-    | _ -> Failure
+  let mul = "*" and div = "/" in
+  let mul_tok = token mul and div_tok = token div in
+    match sequence [lazy_parse factor; many @@ sequence [choice [atom mul; atom div]; lazy_parse factor]] target position with
+      | Success (tokens, _, p) ->
+          (match tokens with
+            | [Ast _] as ast_list -> Success (ast_list, target, p)
+            | Ast (_ as lhs) :: op :: Ast (_ as rhs) :: tail when op = mul_tok || op = div_tok ->
+                let ast = ref @@ if op = mul_tok then Mul (lhs, rhs) else Div (lhs, rhs) in
+                let rec conv base = function
+                  | [] -> ()
+                  | op :: Ast (_ as r) :: tail when op = mul_tok || op = div_tok ->
+                      base := if op = div_tok then Mul (!base, r) else Div (!base, r);
+                      conv base tail
+                  | _ -> failwith "(term) Fatal Error"
+                in
+                  conv ast tail;
+                  Success ([Ast !ast], target, p)
+            | _ -> Failure)
+      | _ -> Failure
 
 and arithmetic_expr target position =
-  match sequence [term; many @@ sequence [choice [atom "+"; atom "-"]; term]] target position with
-    | Success (tokens, _, p) ->
-        (match tokens with
-          | [Ast _] as ast_list -> Success (ast_list, target, p)
-          | Ast (_ as lhs) :: op :: Ast (_ as rhs) :: tail when op = Token "+" || op = Token "-" ->
-              let ast = ref @@ if op = Token "+" then Add (lhs, rhs) else Sub (lhs, rhs) in
-              let rec conv base = function
-                | [] -> ()
-                | op :: Ast (_ as r) :: tail when op = Token "+" || op = Token "-" ->
-                    base := if op = Token "+" then Add (!base, r) else Sub (!base, r);
-                    conv base tail
-                | _ -> failwith "(arithmetic_expr) Fatal Error"
-              in
-                conv ast tail;
-                Success ([Ast !ast], target, p)
-          | _ -> Failure)
-    | _ -> Failure
+  let add = "+" and sub = "-" in
+  let add_tok = token add and sub_tok = token sub in
+    match sequence [term; many @@ sequence [choice [atom add; atom sub]; term]] target position with
+      | Success (tokens, _, p) ->
+          (match tokens with
+            | [Ast _] as ast_list -> Success (ast_list, target, p)
+            | Ast (_ as lhs) :: op :: Ast (_ as rhs) :: tail when op = add_tok || op = sub_tok ->
+                let ast = ref @@ if op = add_tok then Add (lhs, rhs) else Sub (lhs, rhs) in
+                let rec conv base = function
+                  | [] -> ()
+                  | op :: Ast (_ as r) :: tail when op = add_tok || op = sub_tok ->
+                      base := if op = add_tok then Add (!base, r) else Sub (!base, r);
+                      conv base tail
+                  | _ -> failwith "(arithmetic_expr) Fatal Error"
+                in
+                  conv ast tail;
+                  Success ([Ast !ast], target, p)
+            | _ -> Failure)
+      | _ -> Failure
 
 and logical_expr_and target position =
-  match sequence [arithmetic_expr; many @@ sequence [atom "&&"; arithmetic_expr]] target position with
-    | Success (atomss, _, p) ->
-        (match atomss with
-          | [Ast _] as ast_list -> Success (ast_list, target, p)
-          | Ast (_ as lhs) :: Token "&&" :: Ast (_ as rhs) :: tail ->
-              let ast = ref @@ And (lhs, rhs) in
-              let rec conv base = function
-                | [] -> ()
-                | Token "&&" :: Ast (_ as r) :: tail ->
-                    base := And (!base, r);
-                    conv base tail
-                | _ -> failwith "(logical_expr_and) Fatal Error"
-              in
-                conv ast tail;
-                Success ([Ast !ast], target, p)
-          | _ -> Failure)
-    | _ -> Failure
+  let and_op = "&&" in
+  let and_tok = token and_op in
+    match sequence [arithmetic_expr; many @@ sequence [atom and_op; arithmetic_expr]] target position with
+      | Success (atomss, _, p) ->
+          (match atomss with
+            | [Ast _] as ast_list -> Success (ast_list, target, p)
+            | Ast (_ as lhs) :: op :: Ast (_ as rhs) :: tail when op = and_tok ->
+                let ast = ref @@ And (lhs, rhs) in
+                let rec conv base = function
+                  | [] -> ()
+                  | op :: Ast (_ as r) :: tail when op = and_tok ->
+                      base := And (!base, r);
+                      conv base tail
+                  | _ -> failwith "(logical_expr_and) Fatal Error"
+                in
+                  conv ast tail;
+                  Success ([Ast !ast], target, p)
+            | _ -> Failure)
+      | _ -> Failure
 
 and logical_expr_or () target position =
-  match sequence [logical_expr_and; many @@ sequence [atom "||"; logical_expr_and]] target position with
-    | Success (tokens, _, p) ->
-        (match tokens with
-          | [Ast _] as ast_list -> Success (ast_list, target, p)
-          | Ast (_ as lhs) :: Token "||" :: Ast (_ as rhs) :: tail ->
-              let ast = ref @@ Or (lhs, rhs) in
-              let rec conv base = function
-                | [] -> ()
-                | Token "||" :: Ast (_ as r) :: tail ->
-                    base := Or (!base, r);
-                    conv base tail
-                | _ -> failwith "(logical_expr_or) Fatal Error"
-              in
-                conv ast tail;
-                Success ([Ast !ast], target, p)
-          | _ -> Failure)
-    | _ -> Failure
+  let or_op = "||" in
+  let or_tok = token or_op in
+    match sequence [logical_expr_and; many @@ sequence [atom or_op; logical_expr_and]] target position with
+      | Success (tokens, _, p) ->
+          (match tokens with
+            | [Ast _] as ast_list -> Success (ast_list, target, p)
+            | Ast (_ as lhs) :: op :: Ast (_ as rhs) :: tail when op = or_tok ->
+                let ast = ref @@ Or (lhs, rhs) in
+                let rec conv base = function
+                  | [] -> ()
+                  | op :: Ast (_ as r) :: tail when op = or_tok ->
+                      base := Or (!base, r);
+                      conv base tail
+                  | _ -> failwith "(logical_expr_or) Fatal Error"
+                in
+                  conv ast tail;
+                  Success ([Ast !ast], target, p)
+            | _ -> Failure)
+      | _ -> Failure
