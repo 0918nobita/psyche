4f85e2e338589069233eda9e9adb6065503a516b
0918nobita <nobita.0918@gmail.com>
2019-06-27 12:03:47 +0900

    :art: Apply ocp-indent

--- src/monadic.ml
+++ src/monadic.ml
@@ -4,18 +4,18 @@ let parse (MParser p) = p
 
 let substr str start len =
   let str_len = String.length str in
-    if str_len >= start + len
-      then Some (String.sub str start len)
-      else None
+    if str_len >= start + len then Some (String.sub str start len) else None
 
 let return x = MParser x
 
 let token tok =
   let length = String.length tok in
-    MParser (fun src -> match substr src 0 length with
-      | Some cut when cut = tok ->
-          [(cut, String.sub src length (String.length src - length))]
-      | _ -> [])
+    return (fun src ->
+      match substr src 0 length with
+        | Some cut when cut = tok ->
+            [(cut, String.sub src length (String.length src - length))]
+        | _ ->
+            [] )
 
 (* Function Composition *)
 let ( <.> ) f g x = f @@ g x
@@ -23,7 +23,6 @@ let ( <.> ) f g x = f @@ g x
 let concatMap f = List.(concat <.> map f)
 
 let ( >>= ) p f =
-  MParser (fun src ->
-    parse p src
-      |> concatMap (fun (a, str) -> parse (f a) str))
+  return
+    (fun src -> parse p src |> concatMap (fun (a, str) -> parse (f a) str))
 
