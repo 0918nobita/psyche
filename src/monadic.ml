cf470b596ea21e0be1317560934809610f2286ad
0918nobita <nobita.0918@gmail.com>
2019-06-27 16:56:16 +0900

    :recycle: Refactor monadic.ml

--- src/monadic.ml
+++ src/monadic.ml
@@ -6,11 +6,9 @@ let substr str start len =
   let str_len = String.length str in
     if str_len >= start + len then Some (String.sub str start len) else None
 
-let return x = MParser x
-
 let token tok =
   let length = String.length tok in
-    return (fun src ->
+    MParser (fun src ->
         match substr src 0 length with
           | Some cut when cut = tok ->
               [(cut, String.sub src length (String.length src - length))]
@@ -23,18 +21,22 @@ let ( <.> ) f g x = f @@ g x
 let concatMap f = List.(concat <.> map f)
 
 let ( >>= ) p f =
-  return
-    (fun src ->
-       parse p src
-       |> concatMap (fun (a, str) -> parse (f a) str))
+  MParser
+    (fun src -> parse p src
+                |> concatMap (fun (a, str) -> parse (f a) str))
 
 let ( >> ) m f = m >>= fun _ -> f
 
-let ( <*> ) fs xs = fs >>= fun f -> xs >>= fun x -> return (f x)
+let ( <*> ) precede succeed =
+  MParser
+    (fun src -> parse precede src
+                |> concatMap (fun (f, str) ->
+                    parse succeed str
+                    |> List.map (fun (ast, str') -> (f ast, str'))))
 
 let mparser =
   token "a"
   >>= fun cut ->
-  return (fun src ->
+  MParser (fun src ->
       let result = parse (token "b") src in
         List.map (fun (a, str) -> (cut ^ a, str)) result)
