461809c2824fa51397d441acb124f6497ee96fc4
0918nobita <nobita.0918@gmail.com>
2019-06-28 17:04:45 +0900

    :building_construction: Parser モジュールを削除した

--- src/monadic.ml
+++ src/monadic.ml
@@ -7,77 +7,43 @@ let ( <.> ) f g x = f @@ g x
 
 let concatMap f = List.(concat <.> map f)
 
+type 'a parser = MParser of (string -> ('a * string) list)
 
-module Parser = struct
-  type 'a parser = MParser of (string -> ('a * string) list)
+let parse (MParser p) = p
 
-  let parse (MParser p) = p
-
-  let token tok =
-    let length = String.length tok in
-      MParser (fun src ->
-          match substr src 0 length with
-            | Some cut when cut = tok ->
-                [(cut, String.sub src length (String.length src - length))]
-            | _ ->
-                [] )
-
-
-  (* Functor *)
-
-  let fmap f p =
+let token tok =
+  let length = String.length tok in
     MParser (fun src ->
-        List.map (fun (a, str) -> (f a, str)) @@ (parse p) src)
+        match substr src 0 length with
+          | Some cut when cut = tok ->
+              [(cut, String.sub src length (String.length src - length))]
+          | _ ->
+              [] )
 
+let ( <*> ) precede succeed =
+  MParser
+    (fun src -> parse precede src
+                |> concatMap (fun (f, str) ->
+                  parse succeed str
+                  |> List.map (fun (ast, str') -> (f ast, str'))))
 
-  (* Applicative *)
+let ( <$> ) f p = MParser (fun src ->
+  List.map (fun (a, str) -> (f a, str)) @@ (parse p) src)
 
-  let pure ast = MParser (fun src -> [(ast, src)])
+let return ast = MParser (fun src -> [(ast, src)])
 
-  (** Sequential application *)
-  let ( <*> ) precede succeed =
-    MParser
-      (fun src -> parse precede src
-                  |> concatMap (fun (f, str) ->
-                      parse succeed str
-                      |> List.map (fun (ast, str') -> (f ast, str'))))
+let ( >>= ) p f =
+  MParser
+    (fun src -> parse p src
+                |> concatMap (fun (a, str) -> parse (f a) str))
 
-  let ( <$> ) = fmap
+let ( >> ) m f = m >>= fun _ -> f
 
+let mzero = MParser (fun _ -> [])
 
-  (* Monad *)
+let ( <|> ) p q = MParser (fun src -> parse p src @ parse q src)
 
-  let return = pure
-
-  (**
-    Sequentially compose two actions,
-    passing any value produced by the first as a argument to the second.
-  *)
-  let ( >>= ) p f =
-    MParser
-      (fun src -> parse p src
-                  |> concatMap (fun (a, str) -> parse (f a) str))
-
-  (**
-    Sequentially compose two actions, discarding any value produced by the first,
-    like sequencing operators (such as the semicolon) in imperative languages
-  *)
-  let ( >> ) m f = m >>= fun _ -> f
-
-
-  (* Alternative - A monoid on applicative functors *)
-
-  (** The identity of '<|>' *)
-  let empty = MParser (fun _ -> [])
-
-  (** An associative binary operation *)
-  let ( <|> ) p q = MParser (fun src -> parse p src @ parse q src)
-
-
-  (* MonadPlus - Monads that also support choice and failure *)
-  let mzero = empty
-  let mplus = (<|>)
-end
+let liftA2 f x = (<*>) (f <$> x)
 
 
 let mparser =
