6c59805ba00f5369fbfc5825731a0b1436888f6e
0918nobita <nobita.0918@gmail.com>
2019-06-28 11:28:19 +0900

    :building_construction: monadic.ml でのパーサに関する関数を Parser モジュール内で定義するようにした

--- src/monadic.ml
+++ src/monadic.ml
@@ -1,19 +1,6 @@
-type 'a parser = MParser of (string -> ('a * string) list)
-
-let parse (MParser p) = p
-
 let substr str start len =
   let str_len = String.length str in
     if str_len >= start + len then Some (String.sub str start len) else None
-
-let token tok =
-  let length = String.length tok in
-    MParser (fun src ->
-        match substr src 0 length with
-          | Some cut when cut = tok ->
-              [(cut, String.sub src length (String.length src - length))]
-          | _ ->
-              [] )
 
 (* Function Composition *)
 let ( <.> ) f g x = f @@ g x
@@ -21,59 +8,75 @@ let ( <.> ) f g x = f @@ g x
 let concatMap f = List.(concat <.> map f)
 
 
-(* Functor *)
+module Parser = struct
+  type 'a parser = MParser of (string -> ('a * string) list)
 
-let fmap f p =
-  MParser (fun src ->
-    List.map (fun (a, str) -> (f a str)) @@ (parse p) src)
+  let parse (MParser p) = p
 
-
-(* Applicative *)
-
-(** Sequential application *)
-let ( <*> ) precede succeed =
-  MParser
-    (fun src -> parse precede src
-                |> concatMap (fun (f, str) ->
-                    parse succeed str
-                    |> List.map (fun (ast, str') -> (f ast, str'))))
-
-let ( <$> ) = fmap
+  let token tok =
+    let length = String.length tok in
+      MParser (fun src ->
+          match substr src 0 length with
+            | Some cut when cut = tok ->
+                [(cut, String.sub src length (String.length src - length))]
+            | _ ->
+                [] )
 
 
-(* Monad *)
+  (* Functor *)
 
-(**
-  Sequentially compose two actions,
-  passing any value produced by the first as a argumet to the second.
-*)
-let ( >>= ) p f =
-  MParser
-    (fun src -> parse p src
-                |> concatMap (fun (a, str) -> parse (f a) str))
-
-(**
-  Sequentially compose two actions, discarding any value produced by the first,
-  like sequencing operators (such as the semicolon) in imperative languages
-*)
-let ( >> ) m f = m >>= fun _ -> f
+  let fmap f p =
+    MParser (fun src ->
+        List.map (fun (a, str) -> (f a, str)) @@ (parse p) src)
 
 
-(*
-  Alternative - A monoid on applicative functors
-  MonadPlus - Monads that also support choice and failure
-*)
+  (* Applicative *)
 
-(** The identity of '<|>' *)
-let empty = MParser (fun _ -> [])
+  (** Sequential application *)
+  let ( <*> ) precede succeed =
+    MParser
+      (fun src -> parse precede src
+                  |> concatMap (fun (f, str) ->
+                      parse succeed str
+                      |> List.map (fun (ast, str') -> (f ast, str'))))
 
-(** An associative binary operation *)
-let ( <|> ) p q = MParser (fun src -> parse p src @ parse q src)
+  let ( <$> ) = fmap
+
+
+  (* Monad *)
+
+  (**
+    Sequentially compose two actions,
+    passing any value produced by the first as a argument to the second.
+  *)
+  let ( >>= ) p f =
+    MParser
+      (fun src -> parse p src
+                  |> concatMap (fun (a, str) -> parse (f a) str))
+
+  (**
+    Sequentially compose two actions, discarding any value produced by the first,
+    like sequencing operators (such as the semicolon) in imperative languages
+  *)
+  let ( >> ) m f = m >>= fun _ -> f
+
+
+  (*
+    Alternative - A monoid on applicative functors
+    MonadPlus - Monads that also support choice and failure
+  *)
+
+  (** The identity of '<|>' *)
+  let empty = MParser (fun _ -> [])
+
+  (** An associative binary operation *)
+  let ( <|> ) p q = MParser (fun src -> parse p src @ parse q src)
+end
 
 
 let mparser =
-  token "a"
+  Parser.(token "a"
   >>= fun cut ->
   MParser (fun src ->
       let result = parse (token "b") src in
-        List.map (fun (a, str) -> (cut ^ a, str)) result)
+        List.map (fun (a, str) -> (cut ^ a, str)) result))
