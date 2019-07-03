8036c21984a6c377739bf32793d541122f76c8ec
0918nobita <nobita.0918@gmail.com>
2019-07-03 20:28:14 +0900

    :sparkles: Parser_combinator モジュールを追加した

---
+++ src/parser_combinator.mli
@@ -0,0 +1,27 @@
+val ( <.> ) : ('a -> 'b) -> ('c -> 'a) -> 'c -> 'b
+
+type 'a parser = MParser of (string -> ('a * string) list)
+
+val parse : 'a parser -> string -> ('a * string) list
+
+val token : string -> string parser
+
+val ( <*> ) : ('a -> 'b) parser -> 'a parser -> 'b parser
+
+val ( <$> ) : ('a -> 'b) -> 'a parser -> 'b parser
+
+val return : 'a -> 'a parser
+
+val char : char -> char parser
+
+val ( >>= ) : 'a parser -> ('a -> 'b parser) -> 'b parser
+
+val ( >> ) : 'a parser -> 'b parser -> 'b parser
+
+val ( <|> ) : 'a parser -> 'a parser -> 'a parser
+
+val oneOf : string -> char parser
+
+val many : 'a parser -> 'a list parser
+
+val some : 'a parser -> 'a list parser lazy_t
