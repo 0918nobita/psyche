c881896e9ffe5f69b639353e7037f6e6a705b76e
0918nobita <nobita.0918@gmail.com>
2019-07-04 15:45:30 +0900

    :sparkles: Add src/parser/parser.mli

---
+++ src/parser/parser.mli
@@ -0,0 +1,20 @@
+type ast =
+  | IntLiteral of int
+  | Minus of ast
+  | Add of ast * ast
+  | Sub of ast * ast
+  | Mul of ast * ast
+  | Div of ast * ast
+  | Eq of ast * ast
+  | Ne of ast * ast
+  | Less of ast * ast
+  | LessE of ast * ast
+  | Greater of ast * ast
+  | GreaterE of ast * ast
+  | And of ast * ast
+  | Or of ast * ast
+  | If of ast * ast * ast
+
+exception Syntax_error
+
+val program : string -> ast
