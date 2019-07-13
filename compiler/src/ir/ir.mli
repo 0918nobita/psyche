fcf655d3d73e91fdde9eb004de0a7a7958659d03
0918nobita <nobita.0918@gmail.com>
2019-07-13 20:06:50 +0900

    :fire: Delete compiler directory

--- compiler/src/ir/ir.mli
+++
@@ -1,5 +0,0 @@
-type instruction
-
-val insts_of_expr_ast : Parser.expr_ast -> instruction list
-
-val bin_of_insts : instruction list -> int ref -> int list
--- compiler/test/speed_test.ml
+++
@@ -1,7 +0,0 @@
-let time f =
-  let start = Sys.time () in
-  let res = f () in
-  let end_ = Sys.time () in
-    (end_ -. start, res)
-
-let () = print_endline "スピードテスト"; exit (-1)
