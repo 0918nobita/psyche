7825b87461cc420dc33894c14811d4288b308089
0918nobita <nobita.0918@gmail.com>
2019-07-23 18:21:54 +0900

    :sparkles: wasm モジュールのインターフェースを定義した

---
+++ compiler/wasm.mli
@@ -0,0 +1,19 @@
+type func_sig = { params: int; results: int }
+
+type imported_func = { import_name: string * string; signature: func_sig }
+
+type exported_func = { export_name: string; signature: func_sig; locals: int; code: int list }
+
+type func =
+  | ImportedFunc of imported_func
+  | Func of { signature: func_sig; locals: int; code: int list }
+  | ExportedFunc of exported_func
+
+type mem = { module_name: string * string; limits: bool; initial: int }
+
+type wasm = {
+  functions: func list;
+  memories: mem list
+}
+
+val bin_of_wasm : wasm -> int list
